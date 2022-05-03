76f9038aae57d747598f0bd36f80d9daccb1fad9
Swiftzerr <swiftzermc@gmail.com>
2022-05-03 13:59:52 -0400

    A bunch of stuff but mostly icons

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,28 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        productName: "FCStudios Mod Manager",
+        appId: 'modlauncher.fcstudioshub.com',
+        win: {
+          "target": [
+            "nsis"
+          ],
+          icon: 'public/icon.png',
+          "requestedExecutionLevel": "requireAdministrator"
+        },
+        "nsis": {
+          "installerIcon": "public/icon.ico",
+          "uninstallerIcon": "public/icon.ico",
+          "uninstallDisplayName": "FCStudios Mod Manager",
+          "license": "license.txt",
+          "oneClick": false,
+          "allowToChangeInstallationDirectory": true
+        }
+      },
+    },
+  },
 })
