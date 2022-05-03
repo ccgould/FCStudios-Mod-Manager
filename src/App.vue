70262f39ef3cb0d069c294db2611f41dc307d2f8
Swiftzerr <swiftzermc@gmail.com>
2022-05-03 12:11:03 -0400

    Import all old code to VueJS

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Sidebar username="Swiftzerr"
+           user_pfp="https://data.whicdn.com/images/305354737/original.jpg"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from './components/Sidebar.vue'
+import './style.less'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   Sidebar
+  },
+  mounted() {
+    // Add any external JS you want to import into this list
+    let scripts = [
+        'https://kit.fontawesome.com/b51eaa32e3.js'
+    ]
+
+    for (let i in scripts) {
+      let script = document.createElement('script')
+      script.src = scripts[i]
+      document.body.appendChild(script)
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="sidebar">
+    <div class="profile">
+      <img :src="user_pfp" alt="Profile Picture">
+      <div class="profile-username"> {{ username }} </div>
+    </div>
+    <div class="sidebar-content">
+      <div class="sidebar-2section">Launcher
+        <a class="sidebar-option" id="mods">
+          <i class="fa-solid fa-circle-play"></i>
+          <div>Mods</div>
+        </a>
+
+        <a class="sidebar-option" id="news">
+          <i class="fa-solid fa-newspaper"></i>
+          <div>News</div>
+        </a>
+
+        <a class="sidebar-option" id="settings">
+          <i class="fa-solid fa-gear"></i>
+          <div>Settings</div>
+        </a>
+
+        <a class="sidebar-option" id="downloads">
+          <i class="fa-solid fa-download"></i>
+          <div>Downloads</div>
+        </a>
+      </div>
+      <div class="sidebar-section">Community
+        <a class="sidebar-option" id="account">
+          <i class="fa-solid fa-user"></i>
+          <div>Account</div>
+        </a>
+
+        <a class="sidebar-option" id="discord">
+          <i class="fa-brands fa-discord"></i>
+          <div>Discord</div>
+        </a>
+
+        <a class="sidebar-option" id="website">
+          <i class="fa-brands fa-chrome"></i>
+          <div>Website</div>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App-Sidebar",
+  props: {
+    username: String,
+    user_pfp: String
+  }
+}
+</script>
